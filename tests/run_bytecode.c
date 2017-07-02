a38c24f01270f8640c6fed8c35387f61c17e5d20
Eric Sheng <git@ericsheng.com>
2017-07-02 03:47:00 -0700

    fixed run_bytecode not preserving registers according to cdecl; added stack+esp modification capabilities

--- tests/run_bytecode.c
+++ tests/run_bytecode.c
@@ -16,6 +16,7 @@
 #include <sys/mman.h>
 
 #define __cdecl __attribute__((__cdecl__))
+#define STACK_SIZE 4096
 
 void write_le32(uint8_t *out, uint32_t value) {
     *out++ = value & 0xFF;
@@ -25,41 +26,59 @@ void write_le32(uint8_t *out, uint32_t value) {
 }
 
 void run_and_print(uint8_t * const bytecode, size_t length) {
+    uint8_t prologue[] = {
+        /* 00: */ 0x60,                               // pushad
+        /* 01: */ 0x89, 0x25, 0x00, 0x00, 0x00, 0x00, // mov dword ptr ds:???, esp
+        /* 07: */ 0xBC, 0x00, 0x00, 0x00, 0x00,       // mov esp, ds:???
+    };
     uint8_t epilogue[] = {
-        0x9C,                               // pushfd
-        0x8F, 0x05, 0x00, 0x00, 0x00, 0x00, // pop dword ptr ds:placeholder
-        0x60,                               // pushad
-        0x8F, 0x05, 0x00, 0x00, 0x00, 0x00, // pop dword ptr ds:placeholder
-        0x8F, 0x05, 0x00, 0x00, 0x00, 0x00, // pop dword ptr ds:placeholder
-        0x8F, 0x05, 0x00, 0x00, 0x00, 0x00, // pop dword ptr ds:placeholder
-        0x8F, 0x05, 0x00, 0x00, 0x00, 0x00, // pop dword ptr ds:placeholder
-        0x8F, 0x05, 0x00, 0x00, 0x00, 0x00, // pop dword ptr ds:placeholder
-        0x8F, 0x05, 0x00, 0x00, 0x00, 0x00, // pop dword ptr ds:placeholder
-        0x8F, 0x05, 0x00, 0x00, 0x00, 0x00, // pop dword ptr ds:placeholder
-        0x8F, 0x05, 0x00, 0x00, 0x00, 0x00, // pop dword ptr ds:placeholder
-        0xC3,                               // ret
+        /* 00: */ 0xA3, 0x00, 0x00, 0x00, 0x00,       // mov dword ptr ds:???, eax
+        /* 05: */ 0x89, 0x0D, 0x00, 0x00, 0x00, 0x00, // mov dword ptr ds:???, ecx
+        /* 0B: */ 0x89, 0x15, 0x00, 0x00, 0x00, 0x00, // mov dword ptr ds:???, edx
+        /* 11: */ 0x89, 0x1D, 0x00, 0x00, 0x00, 0x00, // mov dword ptr ds:???, ebx
+        /* 17: */ 0x89, 0x25, 0x00, 0x00, 0x00, 0x00, // mov dword ptr ds:???, esp
+        /* 1D: */ 0x89, 0x2D, 0x00, 0x00, 0x00, 0x00, // mov dword ptr ds:???, ebp
+        /* 23: */ 0x89, 0x35, 0x00, 0x00, 0x00, 0x00, // mov dword ptr ds:???, esi
+        /* 29: */ 0x89, 0x3D, 0x00, 0x00, 0x00, 0x00, // mov dword ptr ds:???, edi
+        /* 2F: */ 0xBC, 0x00, 0x00, 0x00, 0x00,       // mov esp, ds:???
+        /* 34: */ 0x9C,                               // pushfd
+        /* 35: */ 0x8F, 0x05, 0x00, 0x00, 0x00, 0x00, // pop dword ptr ds:???
+        /* 3B: */ 0x61,                               // popad
+        /* 3C: */ 0xC3,                               // ret
     };
 
     uint32_t outputs[9];
-    write_le32(epilogue + 3, (uint32_t) outputs);
-    for (int i = 1; i < 9; ++i) {
-        write_le32(epilogue + 4 + 6 * i, (uint32_t) (outputs + 9 - i));
+    write_le32(epilogue + 0x37, (uint32_t) outputs); // EFLAGS
+    for (int i = 0; i < 8; ++i) {
+        write_le32(epilogue + 0x1 + 6 * i, (uint32_t) (outputs + 1 + i));
     }
 
-    uint8_t *mem = (uint8_t *) mmap(NULL, length + sizeof(epilogue),
+    uint8_t *mem = mmap(NULL, length + sizeof(prologue) + sizeof(epilogue),
             PROT_WRITE | PROT_EXEC, MAP_ANON | MAP_PRIVATE, -1, 0);
-    memcpy(mem, bytecode, length);
-    memcpy(mem + length, epilogue, sizeof(epilogue));
+    uint8_t *stk = mmap(NULL, STACK_SIZE, PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+    memcpy(mem, prologue, sizeof(prologue));
+    memcpy(mem + sizeof(prologue), bytecode, length);
+    memcpy(mem + sizeof(prologue) + length, epilogue, sizeof(epilogue));
+    // modify prologue esp save to point to epilogue in executable memory
+    write_le32(mem + 0x3, (uint32_t) (mem + sizeof(prologue) + length + 0x30));
+    // modify prologue esp mov to point to the "stack"
+    write_le32(mem + 0x8, (uint32_t) (stk + STACK_SIZE));
 
     ((void __cdecl (*)(void)) mem)();
-
-    munmap(mem, length + sizeof(epilogue));
-    mem = NULL;
 
     for (int i = 0; i < 9; ++i) {
         printf("%" PRIX32 " ", outputs[i]);
     }
     putchar('\n');
+
+    for (int i = STACK_SIZE; i -= 4; ) {
+        uint32_t tmp = stk[i] | stk[i + 1] << 8 | stk[i + 2] << 16 | stk[i + 3] << 24;
+        printf("%" PRIX32 " ", tmp);
+    }
+    
+    munmap(stk, STACK_SIZE);
+    munmap(mem, length + sizeof(prologue) + sizeof(epilogue));
+    stk = mem = NULL;
 }
 
 int main(int argc, char *argv[]) {
@@ -70,11 +89,11 @@ int main(int argc, char *argv[]) {
     }
 
     uint8_t set_eflags[] = {
-        0x9C,                                      // pushfd
+        /* 00: */ 0x9C,                                      // pushfd
         // only clear CF, PF, AF, ZF, SF, and OF
-        0x81, 0x24, 0x24, 0x28, 0xF7, 0xFF, 0xFF,  // and dword ptr [esp], 0xFFFFF728
-        0x81, 0x0C, 0x24, 0x00, 0x00, 0x00, 0x00,  // or dword ptr [esp], PLACEHOLDER
-        0x9D,                                      // popfd
+        /* 01: */ 0x81, 0x24, 0x24, 0x28, 0xF7, 0xFF, 0xFF,  // and dword ptr [esp], 0xFFFFF728
+        /* 08: */ 0x81, 0x0C, 0x24, 0x00, 0x00, 0x00, 0x00,  // or dword ptr [esp], ???
+        /* 0F: */ 0x9D,                                      // popfd
     };
     // there are argc - 10 bytes of given bytecode
     uint8_t bytecode[(argc - 10) + sizeof(set_eflags)];
@@ -92,7 +111,7 @@ int main(int argc, char *argv[]) {
             continue;
         }
         if (i == 1) { // EFLAGS
-            write_le32(bytecode + 11, value);
+            write_le32(bytecode + 0xB, value);
         } else {
             bytecode[offset++] = 0xB8 | (i - 2); // mov r32, imm32
             write_le32(bytecode + offset, value);
